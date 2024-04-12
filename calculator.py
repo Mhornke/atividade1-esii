904346eccac8809ee6c3790bd04596ae24acf7c5
Oaluiser <109995434+Oaluiser@users.noreply.github.com>
2024-04-12 09:37:05 -0300

    Adiciona função de multiplicação e muda a linguagem do projeto

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# returns the sum of the two parameters
+def sum(v1, v2):
+    pass
+
+
+# returns the subtraction of the two parameters
+def sub(v1, v2):
+    pass
+
+
+# returns the division of the two parameters
+def div(v1, v2):
+    pass
+
+
+# returns the multiplication of the two parameters
+def mult(v1, v2):
+    return v1 * v2
+
+
+# returns the square root of the received parameter
+def square(v1):
+    pass
+
+
+print(mult(5, 3))
